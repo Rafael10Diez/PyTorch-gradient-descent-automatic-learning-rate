faff56f6dc28bb7445b8f773ea366a6f486e5e84
Rafael10Diez <rafaeldiezs@gmail.com>
2024-11-11 15:44:51 +0100

    Create main.py
    GD optimizer, Tic-Toc Profiler and Rosenbrock function test

---
+++ main.py
@@ -0,0 +1,72 @@
+
+# ------------------------------ Tic-Toc profiler ------------------------------
+
+from time import time
+
+class TicToc:
+   def __init__(self):
+       self.A      = []
+       self.output = []
+       self.t_ref  = time()
+   def tic(self, label=None):
+       t              = time()-self.t_ref
+       self.A.append([t,label])
+       self.output.append(['tic:', label, t])
+   def toc(self, label=None):
+       t              = time()-self.t_ref
+       t0,label_check = self.A.pop()
+       assert label == label_check
+       self.output.append(['toc:', label, t-t0])
+   def print(self):
+       for row in self.output: print(*row)
+  
+# ------------------------------ Optimizer ------------------------------
+
+import torch 
+
+def dynamic_GD_optimizer(cost_function, x, iters_max = 10_000, lr_ini = 1e-8):
+   
+   cost_function(x).backward()
+   
+   Jcost_old = float('inf')
+   lr    = lr_ini + 0. # will grow exponentially
+   iters = 0
+   
+   while (lr > 1e-12):
+       if iters > iters_max:
+           print('WARNING: iteration limit exceeded')
+           break
+       iters   +=  1
+       x.grad  *=  0.
+       cost_function(x).backward()
+       x.data  -= lr * x.grad
+   
+       Jcost_now = float(cost_function(x))
+   
+       if Jcost_now < Jcost_old:
+           lr        *=  1.5
+           x_old      =  x.data + 0. # triggers copy
+           Jcost_old  =  Jcost_now
+       else:
+           lr      *=  0.5
+           x.data  *=  0.
+           x.data  +=  x_old
+   return x
+
+# ------------------------------ Test Case ------------------------------
+
+if __name__ == '__main__':
+   def cost_function(xy, a=1, b=100):
+       x,y = xy
+       return (a-x)**2 + b*(y-x**2)**2
+   x = torch.tensor([2.,2],
+                    dtype=torch.double,
+                    device='cpu', 
+                    requires_grad=True)
+   timer = TicToc()
+   timer.tic('dynamic_GD_optimizer')
+   dynamic_GD_optimizer(cost_function, x, iters_max = 100_000)
+   timer.toc('dynamic_GD_optimizer')
+   timer.print()
+
+   print('\nOptimization residual error (analytical): ', float((x-1).abs().max()))
